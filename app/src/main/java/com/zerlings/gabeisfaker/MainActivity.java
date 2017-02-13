c9a683788ff22341e1a1bb9e5589ad9df9755a4f
Zerlings <smzh369@126.com>
2017-02-13 16:06:13 +0800

    First commit

---
+++ app/src/main/java/com/zerlings/gabeisfaker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zerlings.gabeisfaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
